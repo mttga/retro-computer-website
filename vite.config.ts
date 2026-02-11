4ca8d582621ef765c95b81f4e1a5001d873ccfe9
mttga <matteogallici@yahoo.it>
2026-02-11 01:12:45 +0100

    setup for githubpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/retro-computer-website/",
   plugins: [checker({ typescript: true })],
   worker: {},
   build: {
